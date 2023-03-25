f15764c191d6b6ae23090bdb9bcb7d7fef4a0b42
11mones <mones_leader@hotmail.com>
2023-03-25 12:51:31 +0000

    e

---
+++ server.js
@@ -0,0 +1,39 @@
+
+const express = require('express')
+const movies = require('./data.json')
+const app = express()
+const port = 3001
+
+app.get('/favorite', firstRoutHandler) ;
+function firstRoutHandler(req,res){
+
+    res.send("Welcome to Favorite Page");
+ 
+}
+
+app.get('/', moviesRoutHandler) ;
+function moviesRoutHandler(req,res){
+let arr = [];
+
+    let mov = new movieCon(movies.title , movies.poster_path , movies.overview);
+    arr.push(mov);
+
+
+
+res.json(arr);
+ 
+}
+
+
+function movieCon(title , poster , overview){
+    this.title = title;
+    this. poster= poster;
+    this.overview= overview;
+
+
+
+}
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
